96cf70aedfc4931bc0c0d66d18f1ef26f669e9dd
Guilhem Jéhanno <guilhem.jeh@gmail.com>
2021-05-13 20:53:50 +0200

    [~] - "Nandayo" and "Shutdown" command modified

--- commands/nandayo.js
+++ commands/nandayo.js
@@ -2,6 +2,6 @@ module.exports = {
 	name: "nandayo",
 	description: "nandayo",
 	execute(client, message, args) {
-        message.channel.send("Ehe?! Te Nandayo!", {files: ["./extras/videos/nandayo/ehe-te-nandayo.mp4"]});
+        message.channel.send('"Ehe?!" Te Nandayo!', {files: ["./extras/videos/nandayo/ehe-te-nandayo.mp4"]});
 	},
 };
