436bb0b8d37c416924a7b9f935fb467a0a619e06
Guilhem Jéhanno <guilhem.jeh@gmail.com>
2021-05-14 23:21:15 +0200

    [~] - Removed trailling spaces in emoji command

--- commands/emojis.js
+++ commands/emojis.js
@@ -3,7 +3,6 @@ module.exports = {
 	description: "emojis",
 	execute(client, message, args, command) {
 		try {
-
 			message.channel.send('The emojis do not belong to the developer, \
 			but to Soreko at the base for the forum and the Discord of the game "Genshin Impact" and therefore I have no right to keep them private. \
 			Here is the Twitter link of the author and the image used.\nTwitter of the author: https://twitter.com/soreko\nThe emojis page:',  {files: ["./extras/emojis/paimon_emote.png"]});
